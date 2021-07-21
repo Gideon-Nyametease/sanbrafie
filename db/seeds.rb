e1e05fb47d1b597b66319110ed72e9836d32f97d
Gideon Nyametease <gknyametease@gmail.com>
2021-07-21 13:09:24 +0000

    seed file addition

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(username: "gideon" , email: "gknyametease@gmail.com", password: "Test@1234", password_confirmation: "Test@1234")
+User.create(username: "Beverlyb5" , email: "bervelyb5@gmail.com", password: "Test@1234", password_confirmation: "Test@1234", role_code: "admin")
