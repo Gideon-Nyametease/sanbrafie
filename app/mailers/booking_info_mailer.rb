52fa73de046940c4b77868f47c93fc7e5740991a
Gideon Nyametease <gknyametease@gmail.com>
2021-07-21 13:19:50 +0000

    runtime error fix

--- app/mailers/booking_info_mailer.rb
+++ app/mailers/booking_info_mailer.rb
@@ -2,6 +2,6 @@ class BookingInfoMailer < ApplicationMailer
     def new_booking_info_email
         @booking_info = params[:booking_info]
     
-        mail(to: <ADMIN_EMAIL>, subject: "Your trip has been booked!")
+        mail(to: "kwame.tcu@gmail.com", subject: "Your trip has been booked!")
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(username: "Beverlyb5" , email: "bervelyb5@gmail.com", password: "Test@1234", password_confirmation: "Test@1234", role_code: "admin")
+User.create(othernames:"Beverly",surname:"Booker Armmah",username: "dr_b_test" , email: "bervelyb5@gmail.com", password: "Test@1234", password_confirmation: "Test@1234", role_code: "admin")
