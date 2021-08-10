5788d8e7018786a84ae78f3eacbfb1f3991b4260
Gideon Nyametease <gknyametease@gmail.com>
2021-08-10 13:03:08 +0000

    update UI

--- app/mailers/booking_info_mailer.rb
+++ app/mailers/booking_info_mailer.rb
@@ -1,7 +1,8 @@
 class BookingInfoMailer < ApplicationMailer
     def new_booking_info_email
         @booking_info = params[:booking_info]
-    
-        mail(to: Rails.application.credentials.google[:gmail_username], subject: "Somebody just booked a trip!")
+        
+        # mail(to: Rails.application.credentials.google[:gmail_username], subject: "Somebody just booked a trip!")
+        mail(to: "sanbrafiegroupinc@gmail.com", subject: "Somebody just booked a trip!")
     end
 end
