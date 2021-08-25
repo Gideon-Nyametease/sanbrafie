ebbfcebe98e99982faf4a283a884e24e1848f69a
Gideon Nyametease <gknyametease@gmail.com>
2021-08-25 18:04:53 +0000

    added strip live keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
 require 'stripe'
-if Rails.application.credentials.stripe.present? && Rails.application.credentials.stripe[:secret_key].present?
-    Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
+if Rails.application.credentials.stripe.present? && Rails.application.credentials.stripe[:live][:secret_key].present?
+    Stripe.api_key = Rails.application.credentials.stripe[:live][:secret_key]
 end
